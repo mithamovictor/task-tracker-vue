125cf4ea8a7e289f8d4233311c6b7ce0fae445b8
victor <vickarungaru@gmail.com>
2022-05-21 16:53:14 +0300

    ui layout hardcoded

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ButtonComponent",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      return this.$emit("btn-click");
+    },
+  },
+};
+</script>
